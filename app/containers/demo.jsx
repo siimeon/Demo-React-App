9b3d2121ad999c93aa6d86c76353a018432354e6
Simo Haakana <simo.haakana@eficode.com>
2016-11-14 23:37:11 +0200

    Adding async action example

--- app/containers/demo.jsx
+++ app/containers/demo.jsx
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import moment from 'moment';
+import axios from 'axios';
+import * as todoActions from 'actions/todo';
 
-export default class Demo extends Component {
+class Demo extends Component {
+
+  componentWillMount() {
+    this.props.getAllTodos();
+  }
 
   provide_time() {
     return moment().format('MMMM Do YYYY, h:mm:ss a');
+  }
+
+  showTodos() {
+    const todoCount = this.props.todo.todos.length;
+    const todos = this.props.todo.todos;
+    if (todoCount > 0) {
+      return todos.map(todo => {
+        console.log(todo.title);
+        return <p key={todo.id}>{todo.title}</p>;
+      });
+    }
+    else {
+      return (
+        <p>Sorry no todos :(</p>
+      );
+    }
+
   }
 
   render() {
     const time = this.provide_time();
     return (
       <div>
-        <h1>Hello World!</h1>
-        <h3>{time}</h3>
-        <button>Demo button</button>
+        {this.showTodos()}
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    todo: state.todo,
+  }
+}
+
+export default connect(mapStateToProps, {...todoActions})(Demo);
